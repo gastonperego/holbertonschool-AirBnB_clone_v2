82b7957c0c55ae2ffa4f1e0da6d3fe4ac5f1194d
FrancoDoldan0 <francodoldan23@gmail.com>
2023-11-24 12:10:30 -0800

    add task 8

--- models/city.py
+++ models/city.py
@@ -11,3 +11,4 @@ class City(BaseModel, Base):
     
     state_id = Column("state_id", String(60), ForeignKey('states.id'), nullable=False)
     name = Column("name", String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
