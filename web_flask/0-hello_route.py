6720d01be39562536bac6803152cb57b2d94c9d0
gastonperego <gastonperego@gmail.com>
2023-12-11 23:33:04 +0000

    fix: documentation and pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """First flask web app"""
 
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
-    return("Hello HBNB!")
+    """display text"""
+    return ("Hello HBNB!")
+
 
 app.run(host="0.0.0.0", port="5000")
