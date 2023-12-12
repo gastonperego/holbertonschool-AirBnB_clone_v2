95fbdd200fb652579aab21ac35c6892140fb177f
gastonperego <gastonperego@mail.com>
2023-12-12 13:44:44 +0000

    feat: task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    First flask web app
+"""
+
+from flask import Flask
+"""
+    Create app flask
+"""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+        Returns the string "Hello HBNB!"
+    """
+    return ("Hello HBNB!")
+
+
+@app.route("/", strict_slashes=False)
+def hello1():
+    """
+        Returns the string "Hello HBNB!"
+    """
+    return ("Hello HBNB!")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
