1b0ac5ef582ad5facab192e16484c966b1f6799d
gastonperego <gastonperego@mail.com>
2023-12-12 13:15:16 +0000

    fix: documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""First flask web app"""
-
+"""
+    First flask web app
+"""
 
 from flask import Flask
-"""Import modules"""
 app = Flask(__name__)
-
-
-"""Create an flsk app"""
-
-
-"""Create a route"""
-
-
 @app.route("/", strict_slashes=False)
 def hello():
+    """
+        Returns the string "Hello HBNB!"
+    """
     return ("Hello HBNB!")
 
 
-"""Run the route"""
 
 app.run(host="0.0.0.0", port="5000")
