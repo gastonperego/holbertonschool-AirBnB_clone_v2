30c45fe743390b010e4338c7ad3734ee55eddb99
gastonperego <gastonperego@mail.com>
2023-12-12 15:03:19 +0000

    feat: task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    First flask web app
+"""
+
+from flask import Flask
+"""
+    Create app flask
+"""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+        Returns the string "Hello HBNB!"
+    """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello1():
+    """
+        Returns the string "Hello HBNB!"
+    """
+    return ("HBNB!")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """
+        Returns C + the string given"
+    """
+    tex = text.replace("_", " ")
+    return (f"C {tex}")
+
+
+@app.route("/python/", defaults={"text" : "is_cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """
+        Returns C + the string given"
+    """
+    tex = text.replace("_", " ")
+    return (f"Pyhton {tex}")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
