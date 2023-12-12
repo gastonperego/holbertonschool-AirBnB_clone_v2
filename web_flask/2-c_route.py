1f4c7eaf0f380c80af9bd8b4d510f60f9c983610
gastonperego <gastonperego@mail.com>
2023-12-12 14:22:44 +0000

    feat: task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    First flask web app
+"""
+
+from flask import Flask
+"""
+    Create app flask
+"""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+        Returns the string "Hello HBNB!"
+    """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello1():
+    """
+        Returns the string "Hello HBNB!"
+    """
+    return ("Hello HBNB!")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """
+        Returns the string "Hello HBNB!"
+    """
+    return (f"C {text.replace("_", " ")}")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
