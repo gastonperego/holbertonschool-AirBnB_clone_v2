8452d50d2023e169cbf1d9f8c475ab2982ddc1a7
gastonperego <gastonperego@mail.com>
2023-12-12 12:58:38 +0000

    fix: documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,7 +3,11 @@
 
 
 from flask import Flask
+"""sja kjs  kasjd akjs dkaj k"""
 app = Flask(__name__)
+"""sja kjs  kasjd akjs dkaj k"""
+
+"""sja kjs  kasjd akjs dkaj k"""
 
 
 @app.route("/", strict_slashes=False)
@@ -11,5 +15,7 @@ def hello():
     """Display text"""
     return ("Hello HBNB!")
 
+
+"""sja kjs  kasjd akjs dkaj k"""
 
 app.run(host="0.0.0.0", port="5000")
