ca1d4baf1ae554a28582f9c96adbdb7770614bd2
gastonperego <gastonperego@mail.com>
2023-12-12 13:23:40 +0000

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,14 +4,18 @@
 """
 
 from flask import Flask
+"""
+    Create app flask
+"""
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def hello():
     """
         Returns the string "Hello HBNB!"
     """
     return ("Hello HBNB!")
-
 
 
 app.run(host="0.0.0.0", port="5000")
